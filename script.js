b4bb0a108b79499ea2f888dc6b24e976dc765eb6
G2 <166596134+Gtwo2@users.noreply.github.com>
2024-05-19 07:42:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function yyyymmdd(date) {
+function fnffn(plus) {
+  function yyyymmdd(date) {
     var year = date.getFullYear().toString().substring(0, 4);
     var month = (date.getMonth() + 1).toString().padStart(2, '0');
     var day = date.getDate().toString().padStart(2, '0');
@@ -6,22 +7,26 @@ function yyyymmdd(date) {
     return year + month + day;
 }
 
-const today = new Date();
+function yyyymmdd2(date) {
+  var year = date.getFullYear();
+  var month = (date.getMonth() + 1).toString().padStart(2, '0');
+  var day = date.getDate().toString().padStart(2, '0');
+  return `${year}년 ${month}월 ${day}일 급식 정보입니다.`;
+}
 
 
-let plus = 0;
+let today = new Date();
+
+var desP = document.getElementById('des');
+desP.textContent = yyyymmdd2(new Date(today.setDate(today.getDate() + plus)));
 let date_string = yyyymmdd(new Date(today.setDate(today.getDate() + plus)));
-console.log(date_string);
 
-const api_key = "0ee0c17caaa7492d873adc0bb8e618e1" //넘어가주세요
+let api_key = "0ee0c17caaa7492d873adc0bb8e618e1" //넘어가주세요
 
 let school_code = "7812097";
 let location_code = "K10";
 
-let grade = 1;
-let cl_ass = 8;
-const lunch_url = `https://open.neis.go.kr/hub/mealServiceDietInfo?TYPE=JSON&ATPT_OFCDC_SC_CODE=${location_code}&SD_SCHUL_CODE=${school_code}&KEY=${api_key}&MLSV_YMD=${date_string}`;
-const time_url = `https://open.neis.go.kr/hub/misTimetable?Type=json&ATPT_OFCDC_SC_CODE=${location_code}&SD_SCHUL_CODE=${school_code}&GRADE=${grade}&CLASS_NM=${cl_ass}&KEY=${api_key}&ALL_TI_YMD=${date_string}`
+let lunch_url = `https://open.neis.go.kr/hub/mealServiceDietInfo?TYPE=JSON&ATPT_OFCDC_SC_CODE=${location_code}&SD_SCHUL_CODE=${school_code}&KEY=${api_key}&MLSV_YMD=${date_string}`;
 
 fetch(lunch_url)
   .then(response => {
@@ -31,24 +36,123 @@ fetch(lunch_url)
     return response.json();
   })
   .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('오류났으니까 김동주한테 문의해주세요. /급식정보오류/', error);
-  });
+    if (data.RESULT && data.RESULT.MESSAGE) {
+      let message = data.RESULT.MESSAGE;
+      if (message === "해당하는 데이터가 없습니다.") {
+        let GoodELEMENT = document.querySelector('.menu');
+        ["오늘은 급식이 없는것 같아요!"].forEach(item => {
+          let p = document.createElement('p');
+          p.id = 'message';
+          p.textContent = item;
+          GoodELEMENT.appendChild(p);
+        });
+      }
+    } else {
+      console.log(data)
+      let ddishNm = data.mealServiceDietInfo[1].row[0].DDISH_NM;
+      let orplcInfo = data.mealServiceDietInfo[1].row[0].ORPLC_INFO;
+      let calInfo = data.mealServiceDietInfo[1].row[0].CAL_INFO;
+      let ntrInfo = data.mealServiceDietInfo[1].row[0].NTR_INFO;
 
-  fetch(time_url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('네트워크 오류');
+      var GoodELEMENT = document.querySelector('.btn');
+
+      var p = document.createElement('button');
+      p.id = 'toggle';
+      p.innerHTML = '자세히 보기';
+      GoodELEMENT.appendChild(p);
+
+      var GoodELEMENT = document.querySelector('.menu');
+
+      var p = document.createElement('p');
+      var ff = document.createElement('p');
+      ff.id = 'ch';
+      ff.innerHTML = '메뉴';
+      p.innerHTML = ddishNm;
+
+      GoodELEMENT.appendChild(ff);
+      GoodELEMENT.appendChild(p);
+
+      var GoodELEMENT = document.querySelector('.data');
+
+      var p = document.createElement('p');
+      var ff = document.createElement('p');
+      ff.id = 'ch';
+      ff.innerHTML = '칼로리';
+      p.innerHTML = calInfo;
+      GoodELEMENT.appendChild(ff);
+      GoodELEMENT.appendChild(p);
+
+      var p = document.createElement('p');
+      var ff = document.createElement('p');
+      ff.id = 'ch';
+      ff.innerHTML = '영양 정보';
+      p.innerHTML = ntrInfo;
+      GoodELEMENT.appendChild(ff);
+      GoodELEMENT.appendChild(p);
+
+      var p = document.createElement('p');
+      var ff = document.createElement('p');
+      ff.id = 'ch';
+      ff.innerHTML = '원산지 표시';
+      p.innerHTML = orplcInfo;
+      GoodELEMENT.appendChild(ff);
+      GoodELEMENT.appendChild(p);
+
+      let swich = "n";
+        var dataDiv = document.querySelector('.data');
+        var toggle = document.getElementById('toggle');
+        dataDiv.style.display = "none";
+      
+        toggle.addEventListener('click', toggleDataDiv);
+      
+        function toggleDataDiv() {
+          if (swich === "b") {
+            swich = "n";
+            dataDiv.style.display = "none";
+            toggle.textContent = "자세히 보기";
+          } else {
+            swich = "b";
+            dataDiv.style.display = "block";
+            toggle.textContent = "간략히 보기";
+          }
+        }
     }
-    return response.json();
-  })
-  .then(data => {
-    const itrtCntntList = data.misTimetable[1].row.map(item => item.ITRT_CNTNT);
-
-    console.log(itrtCntntList);
   })
   .catch(error => {
-    console.error('오류났으니까 김동주한테 문의해주세요. /시간표오류/', error);
+    let GoodELEMENT = document.querySelector('.menu');
+    ["오류남"].forEach(item => {
+      let p = document.createElement('p');
+      p.id = 'message';
+      p.textContent = item;
+      GoodELEMENT.appendChild(p);
+    });
   });
+}
+let awesome = 0;
+fnffn(awesome);
+
+const leftBtn = document.getElementById('leftbtn');
+leftBtn.addEventListener('click', () => {
+  awesome -= 1;
+  const menuDiv = document.querySelector('.menu');
+  const btnDiv = document.querySelector('.btn');
+  const dataDiv = document.querySelector('.data');
+
+  menuDiv.innerHTML = ''
+  btnDiv.innerHTML = '';
+  dataDiv.innerHTML = '';
+  fnffn(awesome);
+});
+
+const rightBtn = document.getElementById('rightbtn');
+rightBtn.addEventListener('click', () => {
+  awesome += 1;
+  const menuDiv = document.querySelector('.menu');
+  const btnDiv = document.querySelector('.btn');
+  const dataDiv = document.querySelector('.data');
+
+  menuDiv.innerHTML = ''
+  btnDiv.innerHTML = '';
+  dataDiv.innerHTML = '';
+  fnffn(awesome);
+});
